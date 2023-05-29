81f2e4f418403814820e72c98afab4ab090ce18c
SSP <sungsu930@naver.com>
2023-05-29 21:21:52 +0900

    test7

--- test/src/main/java/com/web/root/member/dto/memberDTO.java
+++ test/src/main/java/com/web/root/member/dto/memberDTO.java
@@ -3,4 +3,18 @@ package com.web.root.member.dto;
 public class memberDTO {
 	private String id;
 	private String pwd;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	
+	
 }
